2ff41cddc6132bf91114dde8dc69d9fed89917f1
Cyranowski, Mariusz (EXT) <mariusz.cyranowski@skanska.pl>
2015-11-27 16:30:11 +0100

    dodaje zapisywanie nowego testu i wyswietlanie go na glownym ekranie, serwis do generowania guidow

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
 /* global angular */
 'use strict';
 
+
 angular.module('uiTestRunner', ['ngRoute', 'ngStorage']);
 
-angular.module('uiTestRunner').
-	controller('MainController', function MainController($localStorage) {
-		this.search = $localStorage.search;
-		
+angular.module('uiTestRunner')
+	.service("GuidGeneratorService", function () {
+		return function guid() {
+			function s4() {
+				return Math.floor((1 + Math.random()) * 0x10000)
+					.toString(16)
+					.substring(1);
+			}
+			return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+				s4() + '-' + s4() + s4() + s4();
+		};
 	})
-	.controller("NewTestController", function NewTestController($localStorage) {
-		$localStorage.search = new Date().toString();
+	.controller('MainController', function MainController($localStorage) {
+		this.Tests = $localStorage.Tests;
 	})
-	.config(function($routeProvider, $locationProvider) {
+	.controller("NewTestController", function NewTestController($localStorage, $location, GuidGeneratorService) {
+		$localStorage.Tests = $localStorage.Tests || [];
+		var self = this;
+		this.Save = function () {
+			$localStorage.Tests.push({
+				Guid: GuidGeneratorService(),
+				Url: self.Url,
+				Code: self.Code,
+				Name: self.Name,
+				Tags: self.Tags,
+			});
+			$location.path("Home");
+		};
+	})
+	.config(function ($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/Home', {
-				templateUrl : 'main.html',
-				controller : 'MainController',
-				controllerAs : 'vm'
+				templateUrl: 'main.html',
+				controller: 'MainController',
+				controllerAs: 'vm'
 			})
 			.when('/New', {
-				templateUrl : 'edit.html',
-				controller : 'NewTestController',
-				controllerAs : 'vm'	
+				templateUrl: 'edit.html',
+				controller: 'NewTestController',
+				controllerAs: 'vm'
 			})
 			.otherwise({
 				redirectTo: '/Home'
 			});
-		
+
 	});
 	
