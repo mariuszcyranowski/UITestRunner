310bf994fb252321c2a9d31fac027136a75c52b7
Bielski, Hubert (EXT) <hubert.bielski@skanska.pl>
2015-12-04 16:37:48 +0100

    dodanie testRepository, css popupu, refactoring zapisywania, usuniecie kontrolera dodawania nowego testu

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ run server inside this directory with command:
 python -m SimpleHTTPServer
 
 do zrobienia:
-kontroler do edycji i dodawania, który otrzymuje obiekt do zapisu.
-stworzenie interjesu do danych zastepujacego localStorage
-
+kod testu
+edytor tagów
--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 angular.module('uiTestRunner', ['ngRoute', 'ngStorage', 'md.data.table', 'ngMaterial']);
 
 angular.module('uiTestRunner')
-	.service("GuidGeneratorService", function () {
+	.service("guidGeneratorService", function () {
 		return function guid() {
 			function s4() {
 				return Math.floor((1 + Math.random()) * 0x10000)
@@ -16,21 +16,47 @@ angular.module('uiTestRunner')
 				s4() + '-' + s4() + s4() + s4();
 		};
 	})
-	.controller('MainController', function MainController($localStorage, $location, $mdDialog) {
-		this.Tests = $localStorage.Tests || ($localStorage.Tests = []);
-		this.add = function (event) {
-			$mdDialog.show({
-				clickOutsideToClose: true,
-				controller: 'NewTestController',
-				controllerAs: 'vm',
-				focusOnOpen: false,
-				targetEvent: event,
-				templateUrl: 'edit.html',
-			}).then(function (test) {
-				console.log(test);
-			});
-		}
+	.service('testRepository', function testRepository($localStorage, guidGeneratorService) {
+		$localStorage.Tests = $localStorage.Tests || [];
+
+		var all = function () {
+			return $localStorage.Tests;
+		};
+
+		var save = function (test) {
+			var testToSave;
+
+			if (test.Guid) {
+				testToSave = $localStorage.Tests.find(function (t) {
+					return t.Guid === test.Guid;
+				});
+			} else {
+				testToSave = {
+					Guid: guidGeneratorService()
+				};
+				$localStorage.Tests.push(testToSave);
+			}
+			testToSave.Url = test.Url;
+			testToSave.Code = test.Code;
+			testToSave.Name = test.Name;
+			testToSave.Tags = test.Tags;
+		};
+
+		var remove = function (test) {
+			var idx = $localStorage.Tests.indexOf(test);
+			$localStorage.Tests.splice(idx, 1);
+		};
+
+		return {
+			all: all,
+			save: save,
+			remove: remove
+		};
+	})
+	.controller('MainController', function MainController(testRepository, $mdDialog) {
+		this.Tests = testRepository.all();
 		this.edit = function (event, test) {
+			event.stopPropagation();
 			$mdDialog.show({
 				clickOutsideToClose: true,
 				controller: 'EditTestController',
@@ -47,74 +73,36 @@ angular.module('uiTestRunner')
 		}
 
 		this.remove = function (test) {
-			var idx = $localStorage.Tests.indexOf(test);
-			$localStorage.Tests.splice(idx, 1);
+			testRepository.remove(test);
 		}
 	})
-	.controller("NewTestController", function NewTestController($localStorage, $location, GuidGeneratorService, $mdDialog) {
-		$localStorage.Tests = $localStorage.Tests || [];
-		var self = this;
-		this.Cancel = function () {
-			$mdDialog.hide();
-		};
-		this.Save = function () {
-			var test = {
-				Guid: GuidGeneratorService(),
-				Url: self.Url,
-				Code: self.Code,
-				Name: self.Name,
-				Tags: self.Tags,
-			};
-			$localStorage.Tests.push(test);
-			$mdDialog.hide(test);
-		};
-	})
-	.controller("EditTestController", function EditTestController($localStorage, test, $location, $mdDialog) {
+	.controller("EditTestController", function EditTestController(testRepository, test, $mdDialog) {
 		var self = this;
 		angular.extend(this, test);
 		self.Cancel = function () {
 			$mdDialog.hide();
 		};
 		self.Save = function () {
-			var testToUpdate = $localStorage.Tests.find(function (t) {
-				return t.Guid === self.Guid;
-			});
-			testToUpdate.Url = self.Url;
-			testToUpdate.Code = self.Code;
-			testToUpdate.Name = self.Name;
-			testToUpdate.Tags = self.Tags;
-
+			var testToUpdate = {
+				Guid: self.Guid,
+				Url: self.Url,
+				Code: self.Code,
+				Name: self.Name,
+				Tags: self.Tags,
+			};
+			testRepository.save(testToUpdate);
 			$mdDialog.hide(testToUpdate);
 		};
 	})
-	.config(function ($routeProvider, $locationProvider) {
+	.config(function ($routeProvider) {
 		$routeProvider
 			.when('/Home', {
 				templateUrl: 'main.html',
 				controller: 'MainController',
 				controllerAs: 'vm'
 			})
-			.when('/New', {
-				templateUrl: 'edit.html',
-				controller: 'NewTestController',
-				controllerAs: 'vm'
-			})
-			.when('/Edit/:guid', {
-				templateUrl: 'edit.html',
-				controller: 'EditTestController',
-				controllerAs: 'vm',
-				resolve: {
-					test: function ($localStorage, $route) {
-						var guid = $route.current.params.guid;
-						return $localStorage.Tests.find(function (test) {
-							return test.Guid === guid;
-						});
-					}
-				}
-			})
 			.otherwise({
 				redirectTo: '/Home'
 			});
-
 	});
 	
